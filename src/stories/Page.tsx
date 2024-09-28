2e7a86b551cfd0d3e3b1e259acec2a13c67793c2
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2024-09-28 21:55:53 +0300

    page

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { IComponents } from '../lib/models/pageModels/pageModel';
 import { WebConstructor } from '../lib/blocks/WebConstructor';
 import { Button, ScreenSize } from 'alex-evo-sh-ui-kit';
@@ -19,20 +19,6 @@ export const Page: React.FC<IPage> = ({data, dialogs = [], menu = []}) => {
 
   const [vis, setVis] = useState<boolean>(false)
 
-  const [c, setc] = useState<IComponents>(data)
-
-  // const sert = (flag: boolean = false) => {
-  //   setc(prev=>({...prev, value:{...prev.value, value:prev.value.value.map((item, index, arr)=>{
-  //     if(arr.length - 1 === index)
-  //       return({...item, value:{...item.value, value: flag}})
-  //     return item
-  //   })}}))
-  // }
-
-  useEffect(()=>{
-    console.log(c)
-  },[c])
-
   const systemCall = (name:string, ...arg:any) => {
     console.log('systemCall', name, arg)
   }
@@ -41,11 +27,9 @@ export const Page: React.FC<IPage> = ({data, dialogs = [], menu = []}) => {
     <div>
       <div id='modalRoot'></div>
       <Button onClick={()=>setVis(true)}>test</Button>
-      {/* <Button onClick={()=>sert(false)}>test2</Button>
-      <Button onClick={()=>sert(true)}>test2</Button> */}
       {
         vis && <WebConstructor 
-        data={c} 
+        data={data} 
         menu={menu}
         dialogs={dialogs}
         fetchFunction={fatchF} 
