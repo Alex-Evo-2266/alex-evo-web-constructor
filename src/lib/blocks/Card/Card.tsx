4e504bcde4baab5d3e6f20d5be0d50d65820d2a5
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2024-11-15 21:10:30 +0300

    gap

--- src/lib/blocks/Card/Card.tsx
+++ src/lib/blocks/Card/Card.tsx
@@ -2,36 +2,40 @@ import {Card as CD} from 'alex-evo-sh-ui-kit'
 import { ICard } from '../../models/pageModels/pageModel'
 import { Component } from '../Component/Component'
 import { useAction } from '../../helper/actionHendler'
+import { GapComponent } from '../../utilsComponents/gapComponent'
 
 export const Card:React.FC<ICard> = (data) => {
 
     const {actionHendler} = useAction()
 
     return(
-        <CD 
-            style={{
-                width: data.option?.width && `${data.option?.width}px`,
-                height: data.option?.height && `${data.option?.height}px`,
-                backgroundColor: data.option?.backgroundColor,
-                color: data.option?.color,
-                padding: data.option?.padding,
-                margin: data.option?.margin,
-                borderRadius:data.option?.borderRadius && `${data.option?.borderRadius}px`,
-                display: 'flex',
-                justifyContent:
-                (data.option?.pozition == "center")?"center":
-                (data.option?.pozition == "left")?"start":
-                (data.option?.pozition == "right")?"end":
-                undefined
-            }}
-            header={data.label} 
-            imgSrc={data.img} 
-            onClick={()=>actionHendler(data.action)}>
-            {
-                (data.value)?
-                <Component data={data.value}/>:
-                null
-            }
-        </CD>
+        <GapComponent>
+            <CD 
+                style={{
+                    width: data.option?.width && `${data.option?.width}px`,
+                    height: data.option?.height && `${data.option?.height}px`,
+                    backgroundColor: data.option?.backgroundColor,
+                    color: data.option?.color,
+                    padding: data.option?.padding,
+                    margin: data.option?.margin,
+                    borderRadius:data.option?.borderRadius && `${data.option?.borderRadius}px`,
+                    display: 'flex',
+                    justifyContent:
+                    (data.option?.pozition == "center")?"center":
+                    (data.option?.pozition == "left")?"start":
+                    (data.option?.pozition == "right")?"end":
+                    undefined
+                }}
+                header={data.label} 
+                imgSrc={data.img} 
+                onClick={()=>actionHendler(data.action)}>
+                {
+                    (data.value)?
+                    <Component data={data.value}/>:
+                    null
+                }
+            </CD>
+        </GapComponent>
+        
     )
 }
